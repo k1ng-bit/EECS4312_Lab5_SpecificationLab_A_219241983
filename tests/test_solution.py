19ab99ad099cb28f70669541479cef1dbb79eebe
k1ng-bit <111671309+k1ng-bit@users.noreply.github.com>
2026-02-09 17:35:54 -0500

    Added 5 extra tests
    added 5 extra tests to the test bench

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -65,15 +65,50 @@ def test_lunch_break_blocks_all_slots_during_lunch():
 
 """TODO: Add at least 5 additional test cases to test your implementation."""
 
-
-def test_add1():
+def test_buffer_after_event():
     """
-    Functional requirement:
-    Slots overlapping an event must not be suggested.
+    No meeting may start exactly after the event ends and should start only after 15min has passed minimum.
+    """
+    events = [{"start": "9:00", "end": "10:00"}]
+    slots = suggest_slots(events, meeting_duration=30, day="2026-02-01")
+
+    assert "10:00" not in slots
+    assert "10:15" in slots
+
+def test_lunch_time_not_allowed():
+    """
+    Meeting cannot be held under lunch time periods 12:00 to 13:00
     """
     events = [{"start": "10:00", "end": "11:00"}]
     slots = suggest_slots(events, meeting_duration=50, day="2026-02-01")
 
-    assert "10:00" not in slots
-    assert "10:30" not in slots
+    assert "11:15" not in slots
+    assert "12:00" not in slots
+
+def test_event_working_hours():
+    events = [{"start": "08:00", "end": "09:10"}]  # overlaps start of workday
+    slots = suggest_slots(events, meeting_duration=30, day="2026-02-01")
+
+    assert "09:00" not in slots   
+    assert "09:30" in slots 
+
+def test_overlapping_events_merged_buffer():
+    events = [
+        {"start": "10:00", "end": "10:30"},             #overlaping events will be merged
+        {"start": "10:20", "end": "11:00"},
+    ]
+    slots = suggest_slots(events, meeting_duration=30, day="2026-02-01")
+
+    assert "11:00" not in slots
     assert "11:15" in slots
+
+def test_latest_start_time_insideworkhours():   #if meeting time slot starts or ends after work hours, it should not be allowed
+    events = []
+
+    slots1 = suggest_slots(events, meeting_duration=60, day="2026-02-01")
+    assert "16:00" in slots1
+    assert "16:15" not in slots1
+
+    slots2 = suggest_slots(events, meeting_duration=30, day="2026-02-01")
+    assert "16:30" in slots2
+    assert "16:45" not in slots2
